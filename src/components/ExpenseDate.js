ec6649865e5143278f8cbb466101940316ba6f3e
tekutokiari <sqcirc@yahoo.com>
2023-05-23 18:40:11 +0100

    changed month

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -2,7 +2,7 @@ import './ExpenseDate.css';
 
 const ExpenseDate = (props) => {
   const year = props.date.toLocaleString('en-GB', { year: 'numeric' });
-  const month = props.date.toLocaleString('en-GB', { month: '2-digit' });
+  const month = props.date.toLocaleString('en-GB', { month: 'long' });
   const day = props.date.toLocaleString('en-GB', { day: 'numeric' });
   if (props.date === 'N/A') {
     return <div className="expense-date">N/A</div>;
